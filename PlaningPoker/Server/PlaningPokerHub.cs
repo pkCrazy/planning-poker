1811ef0a90ae4de6c66975dbb3df236d10eaddb3
Tamás Kis <kis6724@gmail.com>
2023-08-06 08:18:01 +0200

    Use spaces instead of tabs.

--- PlaningPoker/Server/PlaningPokerHub.cs
+++ PlaningPoker/Server/PlaningPokerHub.cs
@@ -5,45 +5,45 @@ namespace PlaningPoker.Server;
 
 public sealed class PlaningPokerHub : Hub<IPlaningPokerHub>
 {
-	private readonly PlayerStore _store;
+    private readonly PlayerStore _store;
 
-	public PlaningPokerHub(PlayerStore store)
-	{
-		_store = store;
-	}
+    public PlaningPokerHub(PlayerStore store)
+    {
+        _store = store;
+    }
 
     public override Task OnConnectedAsync()
     {
-		_store.Players.TryAdd(Context.ConnectionId, Player.Empty());
+        _store.Players.TryAdd(Context.ConnectionId, Player.Empty());
 
-		return base.OnConnectedAsync();
+        return base.OnConnectedAsync();
     }
 
     public override async Task OnDisconnectedAsync(Exception? exception)
     {
-		_store.Players.Remove(Context.ConnectionId);
+        _store.Players.Remove(Context.ConnectionId);
 
-		await Clients.All.PlayerDisconnected(_store.Players.Values.ToArray());
+        await Clients.All.PlayerDisconnected(_store.Players.Values.ToArray());
 
         await base.OnDisconnectedAsync(exception);
     }
 
     public async Task PlayerConnected(Guid id, string username)
-	{
-		Player player = new(id, username);
-		if (_store.Players.TryGetValue(Context.ConnectionId, out _))
-		{
-			_store.Players[Context.ConnectionId] = player;
-		}
-		else
-		{
-			_store.Players.TryAdd(Context.ConnectionId, player);
-		}
+    {
+        Player player = new(id, username);
+        if (_store.Players.TryGetValue(Context.ConnectionId, out _))
+        {
+            _store.Players[Context.ConnectionId] = player;
+        }
+        else
+        {
+            _store.Players.TryAdd(Context.ConnectionId, player);
+        }
 
-		await Clients.All.ReceivePlayerConnected(_store.Players.Values.ToArray());
-	}
+        await Clients.All.ReceivePlayerConnected(_store.Players.Values.ToArray());
+    }
 
-	public async Task Vote(Guid id, int vote)
-	{
-	}
+    public async Task Vote(Guid id, int vote)
+    {
+    }
 }
--- PlaningPoker/Server/PlayerStore.cs
+++ PlaningPoker/Server/PlayerStore.cs
@@ -4,5 +4,5 @@ namespace PlaningPoker.Server;
 
 public sealed class PlayerStore
 {
-	public Dictionary<string, Player> Players { get; } = new(10);
+    public Dictionary<string, Player> Players { get; } = new(10);
 }
