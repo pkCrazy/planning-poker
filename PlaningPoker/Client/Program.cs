9f76271fb4861b7459a54969486414cc92bf21c7
Tamás Kis <kis6724@gmail.com>
2023-08-19 10:06:27 +0200

    Store the player’s name in local storage

--- PlaningPoker/Client/Program.cs
+++ PlaningPoker/Client/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components.Web;
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PlaningPoker.Client;
 
@@ -7,6 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
 
